cd9169d80a2d6b011a6fa2f4ed9c888465f93fa7
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-04-12 20:03:55 +0100

    feat: cors error added

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
