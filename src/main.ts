e43865c254fce11bf29ec094146b0aa7f13d3225
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-05-23 12:12:34 +0100

    feat: fay hub link added to the home page

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://fays-hub-v3-git-master-skippergoroyegmailcoms-projects.vercel.app/'], 
+    origin: ['http://localhost:3000', 'https://fays-hub-v3.vercel.app/'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
