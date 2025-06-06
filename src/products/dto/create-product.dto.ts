e91944c7f5af7f4b1ad69d6b4c64e59faa91f61a
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-06-06 22:34:01 +0100

    feat: description not null again

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -2,12 +2,15 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
+  @IsNotEmpty()
   description: string;
 
 
